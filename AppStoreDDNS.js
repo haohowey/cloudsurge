7154e10bb1c4dfd4be3f3ca03fcfe1055d230270
haohowey <38514659+haohowey@users.noreply.github.com>
2021-01-25 14:26:40 +0800

    Update AppStoreDDNS.js

--- AppStoreDDNS.js
+++ AppStoreDDNS.js
@@ -7,15 +7,15 @@ Appstore Speed up download (By langkhach)
 Dnspod = type=dns,script-path=https://raw.githubusercontent.com/maicoo-l/Surge/master/Script/dnspod.js,script-update-interval=-1
 */
 
-const url = 'http://119.29.29.29/d?dn=' + $domain;
+const url = 'http://223.5.5.5/d?dn=' + $domain;
 if ($network.v4.primaryInterface === 'en0') {
   $httpClient.get(url, function(error, response, data) {
     if (error) {
-      $done({server: '223.5.5.5'});
+      $done({server: '8.8.8.8'});
     } else {
       $done({addresses: data.split(';'), ttl: 600});
     }
   });
 } else {
-  $done({server: '223.5.5.5'});
+  $done({server: '8.8.8.8'});
 }
